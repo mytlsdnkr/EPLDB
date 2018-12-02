2a0d09c55c06884b9af4528f1961f2885f00b2f3
PARK <mytlsdnkr@gmail.com>
2018-12-02 15:30:14 +0900

    find decimal number with EBPA

--- AI/EBPA.c
+++ AI/EBPA.c
@@ -1,256 +1,209 @@
 #include <stdio.h>
 #include <math.h>
-#define e 2.71828
-#define hcount 40
+#include <time.h>
+#include <stdlib.h>
+#define hcount 41
 #define inputbias 1
-#define hbias 1
+#define hbias 1.0
 #define tNum 10000
 
-
-
 double sigmoid(double x){
-   
-   return 1/(1+1/pow(e,x));
+
+    return 1.0/(1.0+exp(-x));
 
 }
 
 double dersigmoid(double x){
-    return (2e-1)/pow(pow(e,2)-x+1,2);
+    return (sigmoid(x)*(1.0-sigmoid(x)));
 }
 
 
 int main(){
-    int result1,result2;
-    int inputlayer[15][15];
-    int hiddenlayer[hcount];
-    int hinj[hcount];
-    double deltainJ[hcount][10];
-    int outputlayer[10];
-    int outiny[10];
-    double v[15][15][hcount];
-    double vCT[15][15][hcount];
+    FILE *fp;
+    int ip[226];
+    double hiddenlayer[hcount];
+    double hinj[hcount];
+    double deltainJ[hcount];
+    double outputlayer[10];
+    double outiny[10];
+    double v[226][hcount];
+    double vCT[226][hcount];
     double w[hcount][10];
     double deltaK[10];
     double deltaJ[hcount];
     double wCT[hcount][10];
-    float a=5;
-    int targetnumber=9;
-    int count=0;
-    double sum=0;
+    double max=0;
+    int answer=0;
+    float a=4.0;
+    int targetnumber[10];
+    int x=0;
+    int num=0;
+    const char *number[10]={"0.txt","1.txt","2.txt","3.txt","4.txt","5.txt","6.txt","7.txt","8.txt","9.txt"};
 
     int i,j,k,s=0;
 
-    for(i=0;i<15;i++){
-        for(j=0;j<15;j++){
-            inputlayer[i][j]=0;
+
+    srand((unsigned)time(NULL));
+ 
+
+    for(i=0;i<=225;i++){
+        for(s=0;s<=hcount-1;s++){
+            v[i][s]=(rand()%2)-0.5;
         }
     }
-
-    for(k=0;k<hcount;k++){
-        hiddenlayer[k]=0;
-        hinj[k]=0;
-
+    for(i=0;i<=hcount-1;i++){
+        for(j=0;j<10;j++){
+            w[i][j]=(rand()%2)-0.5;
+        }
     }
-    for(i=0;i<10;i++){
+     for(i=0;i<10;i++)
         outputlayer[i]=0;
-        outiny[i]=0;
-    }
-// inputlayer initialize to target number
-    inputlayer[2][2]=1;
-    inputlayer[2][3]=1;
-    inputlayer[2][4]=1;
-    inputlayer[2][5]=1;
-    inputlayer[2][6]=1;
-    inputlayer[2][7]=1;
-    inputlayer[2][8]=1;
-    inputlayer[2][9]=1;
-    inputlayer[2][10]=1;
-    inputlayer[3][2]=1;
-    inputlayer[4][2]=1;
-    inputlayer[5][2]=1;
-    inputlayer[5][3]=1;
-    inputlayer[5][4]=1;
-    inputlayer[5][5]=1;
-    inputlayer[5][6]=1;
-    inputlayer[5][7]=1;
-    inputlayer[5][8]=1;
-    inputlayer[5][9]=1;
-    inputlayer[5][10]=1;
-    inputlayer[6][10]=1;
-    inputlayer[7][10]=1;
-    inputlayer[8][10]=1;
-    inputlayer[9][10]=1;
-    inputlayer[8][10]=1;
-    inputlayer[7][10]=1;
-    inputlayer[6][10]=1;
-    inputlayer[5][10]=1;
-    inputlayer[4][10]=1;
-    inputlayer[3][10]=1;
-    inputlayer[2][10]=1;
+    
+	  
+    ip[0]=1;
+    hiddenlayer[0]=1.0;
 
-
-    while(count<tNum){
-
-
-        for(i=0;i<15;i++){
-            for(j=0;j<15;j++){
-                for(s=0;s<hcount;s++){
-                    v[i][j][s]=9.2;
-                }
-
-
+	for(x=0;x<tNum;x++){
+		 for(num=0;num<10;num++){
+            fp=fopen(number[num],"r");
+            for(i=1;i<=225;i++){
+                fscanf(fp,"%d",&ip[i]);
             }
-            
-        }
-        for(i=0;i<hcount;i++){
-            for(j=0;j<10;j++){
-                w[i][j]=9.1;
-            }
-        }
-        while(s<hcount){
-
-            for(i=0;i<15;i++){
-                for(j=0;j<15;j++){
-                       sum+= inputlayer[i][j]*v[i][j][s];
-                }   
-            
-            }
-            
-            hinj[s]=sigmoid(sum+inputbias);
-            hiddenlayer[s]=hinj[s];
-            sum=0;
-            s++;
-
-        }
-        sum=0;
-        s=0;
-        while(s<10){
-            for(i=0;i<hcount;i++){
-                    sum+=hiddenlayer[i]*w[i][s];
-            }
-            outiny[s]=sigmoid(sum+hbias);
-            outputlayer[s]=outiny[s];
-            sum=0;
-            s++;
-        }
-        s=0;
-        sum=0;
+        fclose(fp);
 
         for(i=0;i<10;i++){
-            deltaK[i]=(targetnumber-outputlayer[i])*dersigmoid(outiny[i]);
+            targetnumber[i]=0.0;
         }
+        targetnumber[num]=1.0;
         
-        for(i=0;i<hcount;i++){
-            for(j=0;j<10;j++){
-                wCT[i][j]=a*deltaK[j]*hiddenlayer[i];
-
-            }
-        }
-
-        for(i=0;i<hcount;i++){
-            for(j=0;j<10;j++){
-                deltainJ[i][j]=deltaK[j]*w[i][j];
-
-            }
-        }
-
-        for(i=0;i<hcount;i++){
-            for(j=0;j<10;j++)
-            deltaJ[i]=deltainJ[i][j]*dersigmoid(hinj[i]);
-        }
-
-        for(i=0;i<15;i++){
-            for(j=0;j<15;j++){
-                for(k=0;k<hcount;k++){
-                    vCT[i][j][k]=a*deltaJ[k]*inputlayer[i][j];
-                    
-                }
-            }
-        }
-
-        for(i=0;i<hcount;i++){
-            for(j=0;j<10;j++){
-                w[i][j]=w[i][j]+wCT[i][j];
-            }
-        }
-
-        for(i=0;i<15;i++){
-            for(j=0;j<15;j++){
-                for(k=0;k<hcount;k++){
-                    v[i][j][k]=v[i][j][k]+vCT[i][j][k];
-                }
-            }
-        }
-
-
-
-
-
+        while(outputlayer[num]<=0.97){
         
+        	
 
+        for(j=1;j<=hcount-1;j++){
+            hinj[j]=v[0][j];
+            for(i=1;i<=225;i++){
+                hinj[j]+=(ip[i]*v[i][j]);
+            }
+            hiddenlayer[j]=sigmoid(hinj[j]);
+            
+        }
 
-        count++;
+       
+        for(k=0;k<10;k++){
+            outiny[k]=w[0][k];
+            for(j=1;j<=hcount-1;j++){
+                outiny[k]+=(hiddenlayer[j]*w[j][k]);
+            }
+            outputlayer[k]=sigmoid(outiny[k]);
+         
+        }
         
 
 
 
-
-    }
-    for(i=0;i<15;i++){
-        for(j=0;j<15;j++){
-            if(inputlayer[i][j]==0){
-                printf("■ ");
-            }
-            else{
-                printf("□ ");
-            }
+        for(k=0;k<10;k++){
+            deltaK[k]=(targetnumber[k]-outputlayer[k])*dersigmoid(outiny[k]);
+            wCT[0][k]=a*deltaK[k];
+            for(j=1;j<=hcount-1;j++)
+            	wCT[j][k]=a*deltaK[k]*hiddenlayer[j];
+            
         }
-        printf("\n");
-    }
-
-    printf("\n");
-
-    sum=0;
-
-    for(i=0;i<hcount;i++){
-        for(j=0;j<10;j++){
-            sum+=w[i][j];
-        }
-    }
-    //w[i][j]의 평균 값
-
-    sum=sum/400;
-
-    result1=sum;
-
-
-    sum=0;
-
-    for(i=0;i<15;i++){
-        for(j=0;j<15;j++){
-            for(k=0;k<hcount;k++){
-
-                sum+=v[i][j][k];
-            }
-        }
-    }
-
-    //v[i][j][k]의 평균값
-
+        
     
-    sum=sum/9000;
+        for(j=1;j<=hcount-1;j++){
+            deltainJ[j]=0;
+            for(k=0;k<10;k++){
+                deltainJ[j]+=(deltaK[k]*w[j][k]);
+            }
 
-    result2=sum;
+            deltaJ[j]=deltainJ[j]*dersigmoid(hinj[j]);
+        }
+
+        for(j=1;j<=hcount-1;j++){
+            for(i=1;i<=225;i++){
+                vCT[i][j]=a*deltaJ[j]*ip[i];
+
+            }
+
+        }
+
+        for(j=1;j<=hcount-1;j++){
+            for(k=0;k<10;k++){
+                w[j][k]+=wCT[j][k];
+            }
+        }
+
+        for(i=1;i<=225;i++){
+            for(j=1;j<=hcount-1;j++){
+                v[i][j]+=vCT[i][j];
+            }
+        }
+        
+        	
 
 
+		}
+			
+	
 
-    printf("학습된 값:%d\n",(result1+result2)/2);
-
+    }
+	}
+		
     
+    for(num=0;num<10;num++){
+    	max=0;
+    	answer=0;
+    	fp=fopen(number[num],"r");
+    	for(i=1;i<=225;i++)
+    		fscanf(fp,"%d",&ip[i]);
+    	fclose(fp);
+    	s=1;
+    	 while(s<=hcount-1){
+            hinj[s]=v[0][s];
+            for(i=1;i<=225;i++){
+                hinj[s]+=(ip[i]*v[i][s]);
+            }
+            hiddenlayer[s]=sigmoid(hinj[s]);
+            s++;
+        }
+         k=0;
+        while(k<10){
+            outiny[k]=w[0][k];
+            for(i=1;i<=hcount-1;i++){
+                outiny[k]+=(hiddenlayer[i]*w[i][k]);
+            }
+            outputlayer[k]=sigmoid(outiny[k]);
+            if(outputlayer[k]>max){
+            	max=outputlayer[k];
+            	answer=k;
+			}
+		
+           
+            k++;
+        }
+        
+       
+        	for (i = 1; i <= 225; i++) {
+			if (ip[i] == 1)
+				printf("■ ");
+			else
+				printf("□ ");
+			if (i % 15 == 0) 
+                printf("\n");
+		}
+		printf("result : %d", answer);
+		printf("\n\n");
+        
 
-
-
-
+    	
+	}
+    
+		
+	
+    
+        printf("count of training:%d\n",x);
+         
 
     return 0;
 }
