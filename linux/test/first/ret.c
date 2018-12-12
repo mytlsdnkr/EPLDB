60e9ae5e7b957e328317a8191271628c8d58b2e8
PARK <mytlsdnkr@gmail.com>
2018-12-12 15:30:56 +0900

    recent

--- linux/test/first/ret.c
+++ linux/test/first/ret.c
@@ -20,21 +20,56 @@ Q: Complete the get_the_n_th_sunday_of_the_year()
 // complete this function
 int get_the_n_th_sunday_of_the_year(int year, int n_th)
 {
-	struct tm tm_a;
-	time_t tv_a;
-	int day;
+    struct tm init[12];
+    struct tm *after[12];
+    struct tm *formday[12];
+    int mday[12],wday[12],i;
+
+	int day=0;
+    time_t init_time[12];
+    time_t second_time[12];
+
+    for(i=0;i<12;i++){
+        memset(&init[i],0,sizeof(struct tm));
+        init[i].tm_year=year-1900;
+        init[i].tm_mon=i;
+        init[i].tm_mday=1;
+        init_time[i]=mktime(&init[i]);
+        after[i]=localtime(&init_time[i]);
+        wday[i]=after[i]->tm_wday;
+        memset(&init[i],0,sizeof(struct tm));
+        init[i].tm_year=year-1900;
+        init[i].tm_mon=i+1;
+        init[i].tm_mday=1;
+        second_time[i]=mktime(&init[i])-1;
+        formday[i]=localtime(&second_time[i]);
+        mday[i]=formday[i]->tm_mday;
+    }
+    int count=0;
+
+        printf("%d\n",after[0]->tm_mday);
+    int j=0;
+    for(i=0;i<12;i++){
+        for(j=1;j<=mday[i];j++){
+            if(after[i]->tm_wday==0){
+                count++;
+                if(count==n_th){
+                    return day;
+                }
+            }
+            after[i]->tm_mday++;
+            init_time[i]=mktime(after[i]);
+            after[i]=localtime(&init_time[i]);
+            if(after[i]->tm_wday==0){
+                count++;
+                if(count==n_th){
+                    return day;
+                }
+            }
+        day+=j+1;
+        }
+    }
 	
-	memset(&tm_a, 0, sizeof(struct tm));
-
-	//----------------->
-
-	// write your codes here only
-	
-
-
-
-
-	//<----------------
 	
 
 	return day;
@@ -64,7 +99,7 @@ int main(int argc, char **argv)
 	printf("The first sunday of %04d_%02d is [%2d]\n", year, n_th, get_the_n_th_sunday_of_the_year(year, n_th));
 
 
-	year = 2017; n_th=10;
+	year = 2017; n_th=3;
 	printf("The first sunday of %04d_%02d is [%2d]\n", year, n_th, get_the_n_th_sunday_of_the_year(year, n_th));
 
 
